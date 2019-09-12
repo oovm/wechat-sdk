0eb63805b47f9f3f0a170dc2fc718a7cbb807af6
aster <aster@vers.site>
2019-09-12 03:03:19 +0000

    🔧 Rebuild hexo-plugin-live2d browser bundle

--- projects/adaptors/hexo-plugin-live2d/browser/doki-live2d-hexo.js
+++ projects/adaptors/hexo-plugin-live2d/browser/doki-live2d-hexo.js
@@ -1,3 +1,4 @@
+"use strict";
 (() => {
     // ../../live2d-widget/src/chrome.ts
     var DEFAULT_WELCOME = [
@@ -6375,160 +6376,6 @@ fn fs_main(input: VsOut) -> @location(0) vec4f {
         }
     }
 
-    // ../../live2d/src/motion/types.ts
-    var MotionPriority = {
-        none: 0,
-        idle: 1,
-        normal: 2,
-        force: 3,
-    };
-
-    // ../../live2d/src/motion/parse-motion3.ts
-    var SEGMENT_KIND = {
-        0: "linear",
-        1: "bezier",
-        2: "stepped",
-        3: "inverseStepped",
-    };
-    function parseMotion3(json) {
-        if (!json || typeof json !== "object") {
-            throw new Error(
-                "@doki-land/live2d: motion3.json root must be an object",
-            );
-        }
-        const root = json;
-        const version = Number(root.Version ?? 3);
-        const meta = root.Meta;
-        if (!meta || typeof meta !== "object") {
-            throw new Error("@doki-land/live2d: motion3.json missing Meta");
-        }
-        const m = meta;
-        const duration = num(m.Duration, "Meta.Duration");
-        const fps = num(m.Fps, "Meta.Fps");
-        const loop = m.Loop === true;
-        const areBeziersRestricted = m.AreBeziersRestricted !== false;
-        const fadeInTime = optionalNum(m.FadeInTime) ?? 0;
-        const fadeOutTime = optionalNum(m.FadeOutTime) ?? 0;
-        const curvesRaw = root.Curves;
-        if (!Array.isArray(curvesRaw)) {
-            throw new Error("@doki-land/live2d: motion3.json missing Curves");
-        }
-        const curves = curvesRaw.map((c, i) => parseCurve(c, i));
-        const userData = [];
-        if (Array.isArray(root.UserData)) {
-            for (const item of root.UserData) {
-                if (!item || typeof item !== "object") continue;
-                const u = item;
-                if (typeof u.Time === "number" && typeof u.Value === "string") {
-                    userData.push({ time: u.Time, value: u.Value });
-                }
-            }
-            userData.sort((a, b) => a.time - b.time);
-        }
-        return {
-            version,
-            duration,
-            fps,
-            loop,
-            areBeziersRestricted,
-            fadeInTime,
-            fadeOutTime,
-            curves,
-            userData,
-        };
-    }
-    function parseCurve(raw, index) {
-        if (!raw || typeof raw !== "object") {
-            throw new Error(`@doki-land/live2d: Curves[${index}] invalid`);
-        }
-        const c = raw;
-        const target = c.Target;
-        const id = c.Id;
-        if (typeof target !== "string" || typeof id !== "string") {
-            throw new Error(
-                `@doki-land/live2d: Curves[${index}] needs Target/Id`,
-            );
-        }
-        if (
-            target !== "Parameter" &&
-            target !== "PartOpacity" &&
-            target !== "Model"
-        ) {
-            throw new Error(
-                `@doki-land/live2d: Curves[${index}] unknown Target ${target}`,
-            );
-        }
-        const segmentsFlat = c.Segments;
-        if (!Array.isArray(segmentsFlat) || segmentsFlat.length < 2) {
-            throw new Error(
-                `@doki-land/live2d: Curves[${index}] empty Segments`,
-            );
-        }
-        const numbers = segmentsFlat.map((n, j) => {
-            if (typeof n !== "number" || !Number.isFinite(n)) {
-                throw new Error(
-                    `@doki-land/live2d: Curves[${index}].Segments[${j}] not a number`,
-                );
-            }
-            return n;
-        });
-        return {
-            target,
-            id,
-            fadeInTime: optionalNum(c.FadeInTime),
-            fadeOutTime: optionalNum(c.FadeOutTime),
-            segments: parseSegments(numbers, index),
-        };
-    }
-    function parseSegments(flat, curveIndex) {
-        let i = 0;
-        const p0 = { time: flat[i++], value: flat[i++] };
-        const out = [];
-        let prev = p0;
-        while (i < flat.length) {
-            const kindId = flat[i++];
-            const kind = SEGMENT_KIND[kindId];
-            if (!kind) {
-                throw new Error(
-                    `@doki-land/live2d: Curves[${curveIndex}] unknown segment ${kindId}`,
-                );
-            }
-            if (kind === "bezier") {
-                if (i + 5 >= flat.length) {
-                    throw new Error(
-                        `@doki-land/live2d: Curves[${curveIndex}] truncated bezier`,
-                    );
-                }
-                const p1 = { time: flat[i++], value: flat[i++] };
-                const p2 = { time: flat[i++], value: flat[i++] };
-                const p3 = { time: flat[i++], value: flat[i++] };
-                out.push({ kind, p0: prev, p1, p2, p3 });
-                prev = p3;
-            } else {
-                if (i + 1 >= flat.length) {
-                    throw new Error(
-                        `@doki-land/live2d: Curves[${curveIndex}] truncated ${kind}`,
-                    );
-                }
-                const p3 = { time: flat[i++], value: flat[i++] };
-                out.push({ kind, p0: prev, p3 });
-                prev = p3;
-            }
-        }
-        return out;
-    }
-    function num(v, label) {
-        if (typeof v !== "number" || !Number.isFinite(v)) {
-            throw new Error(
-                `@doki-land/live2d: motion3 ${label} must be a number`,
-            );
-        }
-        return v;
-    }
-    function optionalNum(v) {
-        return typeof v === "number" && Number.isFinite(v) ? v : void 0;
-    }
-
     // ../../live2d/src/motion/evaluate-curve.ts
     function evaluateMotion3(clip, timeSeconds) {
         const t = clamp(timeSeconds, 0, clip.duration);
@@ -6620,6 +6467,14 @@ fn fs_main(input: VsOut) -> @location(0) vec4f {
         if (n > max) return max;
         return n;
     }
+
+    // ../../live2d/src/motion/types.ts
+    var MotionPriority = {
+        none: 0,
+        idle: 1,
+        normal: 2,
+        force: 3,
+    };
 
     // ../../live2d/src/motion/motion-player.ts
     var MotionPlayer = class {
@@ -6891,6 +6746,152 @@ fn fs_main(input: VsOut) -> @location(0) vec4f {
     function sineEase(t) {
         const x = Math.min(1, Math.max(0, t));
         return 0.5 - 0.5 * Math.cos(x * Math.PI);
+    }
+
+    // ../../live2d/src/motion/parse-motion3.ts
+    var SEGMENT_KIND = {
+        0: "linear",
+        1: "bezier",
+        2: "stepped",
+        3: "inverseStepped",
+    };
+    function parseMotion3(json) {
+        if (!json || typeof json !== "object") {
+            throw new Error(
+                "@doki-land/live2d: motion3.json root must be an object",
+            );
+        }
+        const root = json;
+        const version = Number(root.Version ?? 3);
+        const meta = root.Meta;
+        if (!meta || typeof meta !== "object") {
+            throw new Error("@doki-land/live2d: motion3.json missing Meta");
+        }
+        const m = meta;
+        const duration = num(m.Duration, "Meta.Duration");
+        const fps = num(m.Fps, "Meta.Fps");
+        const loop = m.Loop === true;
+        const areBeziersRestricted = m.AreBeziersRestricted !== false;
+        const fadeInTime = optionalNum(m.FadeInTime) ?? 0;
+        const fadeOutTime = optionalNum(m.FadeOutTime) ?? 0;
+        const curvesRaw = root.Curves;
+        if (!Array.isArray(curvesRaw)) {
+            throw new Error("@doki-land/live2d: motion3.json missing Curves");
+        }
+        const curves = curvesRaw.map((c, i) => parseCurve(c, i));
+        const userData = [];
+        if (Array.isArray(root.UserData)) {
+            for (const item of root.UserData) {
+                if (!item || typeof item !== "object") continue;
+                const u = item;
+                if (typeof u.Time === "number" && typeof u.Value === "string") {
+                    userData.push({ time: u.Time, value: u.Value });
+                }
+            }
+            userData.sort((a, b) => a.time - b.time);
+        }
+        return {
+            version,
+            duration,
+            fps,
+            loop,
+            areBeziersRestricted,
+            fadeInTime,
+            fadeOutTime,
+            curves,
+            userData,
+        };
+    }
+    function parseCurve(raw, index) {
+        if (!raw || typeof raw !== "object") {
+            throw new Error(`@doki-land/live2d: Curves[${index}] invalid`);
+        }
+        const c = raw;
+        const target = c.Target;
+        const id = c.Id;
+        if (typeof target !== "string" || typeof id !== "string") {
+            throw new Error(
+                `@doki-land/live2d: Curves[${index}] needs Target/Id`,
+            );
+        }
+        if (
+            target !== "Parameter" &&
+            target !== "PartOpacity" &&
+            target !== "Model"
+        ) {
+            throw new Error(
+                `@doki-land/live2d: Curves[${index}] unknown Target ${target}`,
+            );
+        }
+        const segmentsFlat = c.Segments;
+        if (!Array.isArray(segmentsFlat) || segmentsFlat.length < 2) {
+            throw new Error(
+                `@doki-land/live2d: Curves[${index}] empty Segments`,
+            );
+        }
+        const numbers = segmentsFlat.map((n, j) => {
+            if (typeof n !== "number" || !Number.isFinite(n)) {
+                throw new Error(
+                    `@doki-land/live2d: Curves[${index}].Segments[${j}] not a number`,
+                );
+            }
+            return n;
+        });
+        return {
+            target,
+            id,
+            fadeInTime: optionalNum(c.FadeInTime),
+            fadeOutTime: optionalNum(c.FadeOutTime),
+            segments: parseSegments(numbers, index),
+        };
+    }
+    function parseSegments(flat, curveIndex) {
+        let i = 0;
+        const p0 = { time: flat[i++], value: flat[i++] };
+        const out = [];
+        let prev = p0;
+        while (i < flat.length) {
+            const kindId = flat[i++];
+            const kind = SEGMENT_KIND[kindId];
+            if (!kind) {
+                throw new Error(
+                    `@doki-land/live2d: Curves[${curveIndex}] unknown segment ${kindId}`,
+                );
+            }
+            if (kind === "bezier") {
+                if (i + 5 >= flat.length) {
+                    throw new Error(
+                        `@doki-land/live2d: Curves[${curveIndex}] truncated bezier`,
+                    );
+                }
+                const p1 = { time: flat[i++], value: flat[i++] };
+                const p2 = { time: flat[i++], value: flat[i++] };
+                const p3 = { time: flat[i++], value: flat[i++] };
+                out.push({ kind, p0: prev, p1, p2, p3 });
+                prev = p3;
+            } else {
+                if (i + 1 >= flat.length) {
+                    throw new Error(
+                        `@doki-land/live2d: Curves[${curveIndex}] truncated ${kind}`,
+                    );
+                }
+                const p3 = { time: flat[i++], value: flat[i++] };
+                out.push({ kind, p0: prev, p3 });
+                prev = p3;
+            }
+        }
+        return out;
+    }
+    function num(v, label) {
+        if (typeof v !== "number" || !Number.isFinite(v)) {
+            throw new Error(
+                `@doki-land/live2d: motion3 ${label} must be a number`,
+            );
+        }
+        return v;
+    }
+    function optionalNum(v) {
+        return typeof v === "number" && Number.isFinite(v) ? v : void 0;
     }
 
     // ../../live2d/src/create-live2d.ts
